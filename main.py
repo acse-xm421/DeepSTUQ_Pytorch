ca737854fa56ec8f2cf8c455c9fd8cb49fbaac8e
acse-fb3ff74a <xuefei.mi22@imperial.ac.uk>
2023-10-19 13:48:36 +0800

    start train and test awa on colab

--- main.py
+++ main.py
@@ -151,17 +151,17 @@ if args.lr_decay:
     lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer,
                                                         milestones=lr_decay_steps,
                                                         gamma=args.lr_decay_rate)
-# # start training
-# trainer = Trainer(model, loss, optimizer, train_loader, val_loader, test_loader, scaler,
-#                   args, lr_scheduler=lr_scheduler)
+# start training
+trainer = Trainer(model, loss, optimizer, train_loader, val_loader, test_loader, scaler,
+                  args, lr_scheduler=lr_scheduler)
 
 
-# ### Pre-traning
-# trainer.train()
+### Pre-traning
+trainer.train()
 
-# # # Save the entire model including its architecture and learned parameters
-# # # torch.save(trainer.model, 'your_model.pth')
-# trainer.save_to_file('trainer.pth')
+# # Save the entire model including its architecture and learned parameters
+# # torch.save(trainer.model, 'your_model.pth')
+trainer.save_to_file('trainer.pth')
 
 trainer_0 = Trainer.load_from_file('trainer.pth')
 # model = torch.load('your_model.pth')
